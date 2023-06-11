63921ff5c292bd5786ca9a8401016df4631e993c
David Amebley <davameb@gmail.com>
2023-06-11 17:21:16 +0300

    Register the AddHttpClient and the AddSqlite services

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 
 var app = builder.Build();
 
